59836421ab575268614f859592f005c0905cc75d
adildotes <adildotes@gmail.com>
2024-03-03 20:07:32 +0500

    modified js

--- script.js
+++ script.js
@@ -4,6 +4,6 @@ document.write("<h1>Hello <span style='color:green;'>JavaScript</span> Planet</h
 
 document.write("<p>I am the <b>master</b> of my own fate.</p>");
 
-document.write("<p>i am the <b>champion</b> of my own soule</p>");
+document.write("<p>i am the <b>champion</b> of my own soule.</p>");
 
-document.write("<p>i am the <b>superhero</b> of my own soule</p>");
+document.write("<p>i am the <b>superhero</b> of my own soule.</p>");
